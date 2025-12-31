9c53551f7e4e520cd932b9830f493a0892c08e43
ChiangChianan <447917006@qq.com>
2025-12-31 19:07:29 +0800

    chore: setup project directory structure

---
+++ thirdparty/protobuf/build/CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/io/io_win32.cc.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/io/io_win32.cc.o: \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/src/google/protobuf/io/io_win32.cc \
+ /usr/include/stdc-predef.h
---
+++ thirdparty/protobuf/build/CMakeFiles/libupb.dir/upb/mini_table/message.c.o.d
@@ -0,0 +1,70 @@
+CMakeFiles/libupb.dir/upb/mini_table/message.c.o: \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/message.c \
+ /usr/include/stdc-predef.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/message.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/enum.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/internal/enum.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/port/def.inc \
+ /usr/include/assert.h /usr/include/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp2.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/libio.h \
+ /usr/include/x86_64-linux-gnu/bits/_G_config.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/port/undef.inc \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/field.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/base/descriptor_constants.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/internal/field.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/internal/size_log2.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/internal/message.h \
+ /home/kinetic/project/mprpc/thirdparty/protobuf/upb/mini_table/internal/sub.h
